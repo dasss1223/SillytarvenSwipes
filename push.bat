a66e39d56e4bf8553fd1d56fef983d97e2138111
dasss1223 <zedss1711@gmail.com>
2025-07-27 16:50:04 +1000

    Delete push.bat

--- push.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd %~dp0
-git remote set-url origin https://github.com/BCRL-tylu/swipes.git >nul 2>&1 || git remote add origin https://github.com/BCRL-tylu/swipes.git
-git push -u origin master
